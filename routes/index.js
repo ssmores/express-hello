2320ffc7e104b9879636b4d19f89bbd0abf1edcd
Stefanie Moy <stefaniesmoy@gmail.com>
2019-07-08 14:03:11 +0000

    Added GET route for new Leads page.

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ var router = express.Router();
 let landing = require('../controllers/landing');
 router.get('/', landing.get_landing);
 router.post('/', landing.submit_lead);
+router.get('/leads', landing.show_leads);
 
 module.exports = router;
